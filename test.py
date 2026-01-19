25cda842a599d9e887e59faf1562456b9df6979e
User <=>
2026-01-19 14:15:33 +0800

    commit testing

--- test.py
+++ test.py
@@ -7,14 +7,50 @@ import numpy as np
 resoultion = (1920,1080)
 
 # Specify video codec
-codec = cv2.VideoWritter_fourcc(*"XVID")
+codec = cv2.VideoWriter_fourcc(*"XVID")
+
 
 #Specify name of Output file
 filename = "Recording.avi"
 
 # Frames rate
-fps = 144.0
+fps = 140.0
 
 #VideoWritter object 
 out = cv2.VideoWriter(filename,codec,fps,resoultion)
+
+
+#Optional 
+#Create an Empty window
+cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
+
+# Resize this window
+cv2.resizeWindow("Live", 480, 270)
+
+while True:
+
+    #Take screenshot using PyAutoGui
+    img = pyautogui.screenshot()
+
+    #Convert the screenshot to a numpy array
+    frame = np.array(img)
+
+    #convert the background from Blue,Green,Red to Red , Green , Blue
+    frame  = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    #Write to output file
+    out.write(frame)
+
+    #Optional : Display the recording screen 
+    cv2.imshow('Live', frame)
+
+    #Stop recording when we press 's
+    if cv2.waitKey(1) == ord('s'):
+        break
+
+#Release the Video Writter
+out.release()
+
+#Destroy window
+cv2.destroyAllWindows()
 
